07bed38197917c2aaf09f8ab8aba2f99fc9de13d
junseon <kimkva@naver.com>
2024-09-11 10:41:04 +0900

    2024-09-11

--- src/main/java/com/keduit/bpro01/repository/MemoRepository.java
+++ src/main/java/com/keduit/bpro01/repository/MemoRepository.java
@@ -1,9 +1,16 @@
 package com.keduit.bpro01.repository;
 
 import com.keduit.bpro01.entity.Memo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+    List<Memo> findByMnoBetweenOrderByMnoDesc(Long from, Long to);
 
+    Page<Memo> findByMnoBetween(Long from, Long to, Pageable pageable);
 
+    void deleteMemoByMnoLessThan(Long num);
 }
--- src/test/java/com/keduit/bpro01/repository/MemoRepositoryTest.java
+++ src/test/java/com/keduit/bpro01/repository/MemoRepositoryTest.java
@@ -7,8 +7,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Commit;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -70,4 +73,55 @@ public class MemoRepositoryTest {
         Page<Memo> result = memoRepository.findAll(pageable);
         System.out.println(result);
     }
+
+    @Test
+    public void testPageDefault2() {
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Memo> result = memoRepository.findAll(pageable);
+        System.out.println(result);
+        System.out.println("--------------------------------");
+        System.out.println("총 페이지 갯수: " + result.getTotalPages());
+        System.out.println("전체 갯수: " + result.getTotalElements());
+        System.out.println("현재 페이지 번호: " + result.getNumber());
+        System.out.println("페이지당 데이터 갯수: " + result.hasNext());
+        System.out.println("시작 페이지 여부: " + result.isFirst());
+        System.out.println("--------------------------------------------");
+        for (Memo memo : result.getContent()){
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    public void testSort() {
+        Sort sort1 = Sort.by("mno").descending();
+        Pageable pageable = PageRequest.of(0, 10, sort1);
+        Page<Memo> result = memoRepository.findAll(pageable);
+        result.get().forEach(memo -> System.out.println(memo));
+    }
+
+    @Test
+    public void testQueryMeth() {
+        List<Memo> list = memoRepository.findByMnoBetweenOrderByMnoDesc(70L, 80L);
+
+        for(Memo memo: list) {
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    public void testQueryMethodWithPagable() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("mno").descending());
+
+        Page<Memo> result = memoRepository.findByMnoBetween(10L, 50L, pageable);
+        result.get().forEach(memo -> System.out.println(memo));
+    }
+
+    @Test
+    @Commit     // delete 의 경우 삭제 테스트 이후 자동으로 롤백되기때문에 commit 으로 실제 테이블 정보를 저장
+    @Transactional
+    public void testDeleteQueryMethod() {
+        memoRepository.deleteMemoByMnoLessThan(10L);
+    }
+
+
 }
